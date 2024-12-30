9ac47ce559b56d796f106566d3559f745228a9b1
AlchemistChief <meistergamer333@gmail.com>
2024-12-30 20:17:45 +0100

    updated youtubedl

--- Server/server.js
+++ Server/server.js
@@ -147,7 +147,7 @@ app.get('/mp4', (req, res) => {
         const filePath = path.resolve(__dirname, 'downloads', fileName);
 
         youtubedl(videoUrl, {
-            format: 'bestaudio',
+            format: 'bestvideo+bestaudio/best',
             noCheckCertificates: true,
             noWarnings: true,
             preferFreeFormats: true,
