d12fdde3b4702fcbc7989f79548ff3d5ec5d916e
AlchemistChief <meistergamer333@gmail.com>
2025-01-03 12:40:00 +0100

    .

--- Server/server.js
+++ Server/server.js
@@ -19,11 +19,6 @@ const corsOptions = {
     optionsSuccessStatus: 200,
 };
 
-// Start streaming response for progress updates
-res.setHeader('Content-Type', 'text/event-stream');
-res.setHeader('Cache-Control', 'no-cache');
-res.setHeader('Connection', 'keep-alive');
-
 // Cookies path
 const cookiesPath = path.resolve(__dirname, 'cookies.txt');
 if (!fs.existsSync(cookiesPath)) {
