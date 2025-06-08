756f507c1641b37ec6f4edaf9e73d47a2d39c9e6
AlchemistChief <meistergamer333@gmail.com>
2025-06-08 22:19:03 +0200

    Added Installation and Start Script

---
+++ START.bat
@@ -0,0 +1,26 @@
+@echo off
+setlocal enabledelayedexpansion
+
+for /f %%a in ('echo prompt $E ^| cmd') do set "ESC=%%a"
+set "RESET=%ESC%[0m"
+set "GOLDCOLOR=%ESC%[1;38;5;220m"
+set "REDCOLOR=%ESC%[1;38;5;196m"
+set "BLUECOLOR=%ESC%[1;38;5;75m"
+set "GREENCOLOR=%ESC%[1;38;5;46m"
+
+set "NODE_PATH=%CD%\server\bin\PortableNode\node.exe"
+
+if exist "%NODE_PATH%" (
+	set "PATH=%CD%\server\bin\PortableNode\;%PATH%"
+	call echo !BLUECOLOR![INFO]!RESET! Node.js found at %NODE_PATH%
+) else (
+	call echo !REDCOLOR![ERROR]!RESET! Node.js not found in PortableNode folder.
+	call echo !REDCOLOR![ERROR]!RESET! Please run Install.bat first.
+	pause
+	exit /b 1
+)
+
+call echo !BLUECOLOR![INFO]!RESET! Starting server with npm start...
+npx npm start
+
+endlocal
